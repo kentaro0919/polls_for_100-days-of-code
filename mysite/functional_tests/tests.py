c85340846e60cbff6fce3a8501f2cfd2b2b800c4
Kentaro Hori <kentaro0919@gmail.com>
2017-10-13 00:10:10 +0900

    make functional test for current  /polls/

---
+++ mysite/functional_tests/tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from django.test import LiveServerTestCase
+from django.test import TestCase
+
+
+class NewPollsTest(LiveServerTestCase):
+    
+    def setUp(self):
+        
+        self.browser = webdriver.Safari()
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_your_first_view(self):
+        self.browser.get(self.live_server_url+"/polls/")
+        #help(self.browser)
+        self.assertEqual("Hello, world. You're at the polls index.",
+                         self.browser.find_element_by_id("tt").text )
