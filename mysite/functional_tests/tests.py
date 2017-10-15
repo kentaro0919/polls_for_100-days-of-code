7da8205f2d9860b1077ccb142aa2d5bc3ae6ac6c
Kentaro Hori <kentaro0919@gmail.com>
2017-10-15 10:40:44 +0900

    fix id issue wth loop

--- mysite/functional_tests/tests.py
+++ mysite/functional_tests/tests.py
@@ -8,21 +8,23 @@ from django.utils import timezone
 class DiteirViewChoiceTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Safari()
         q = Question(question_text="What's new?", pub_date=timezone.now())
         q.save()
-        q.choice_set.create(choice_text='Not much', votes=0)
+        q.choice_set.create(choice_text='choice_text', votes=0)
+        q.save()
+        self.browser = webdriver.Safari()
 
     def tearDown(self):
         self.browser.quit()
 
     def test_detail_views(self):
         q_one = Question.objects.get(pk=1)
+        c_one = q_one.choice_set.all().first()
         self.browser.get(self.live_server_url + "/polls/1/")
         self.assertIn(q_one.question_text,
                       self.browser.find_element_by_id("questiontext").text)
-        self.assertIn(q_one.choice_set.get(pk=1).choice_text,
-                      self.browser.find_elements_by_class_name("choicetext")[0].text)
+        self.assertIn(c_one.choice_text,
+                      self.browser.find_element_by_id(f"choice{c_one.pk }").text)
 
 
 class mayNotworkTest(LiveServerTestCase):
